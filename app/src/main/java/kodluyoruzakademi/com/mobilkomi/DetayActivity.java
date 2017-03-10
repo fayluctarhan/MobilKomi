41edf4545bf0a7dede111a9384cf35735d0e6105
Firat Ayluctarhan <firatayluctarhan@gmail.com>
2017-03-10 14:47:37 +0300

    detay sayfası düzenlendi

--- app/src/main/java/kodluyoruzakademi/com/mobilkomi/DetayActivity.java
+++ app/src/main/java/kodluyoruzakademi/com/mobilkomi/DetayActivity.java
@@ -23,8 +23,20 @@ public class DetayActivity extends AppCompatActivity {
         String tatli = getIntent().getExtras().getString("tat");
         String icecek = getIntent().getExtras().getString("içe");
 
-        TextView test1 = (TextView) findViewById(R.id.test1);
-        test1.setText(baslangic);
+        TextView test1 = (TextView) findViewById(R.id.Baslik1);
+        test1.setText(baslik);
+        TextView test2 = (TextView) findViewById(R.id.TextView1);
+        test2.setText(baslangic);
+        TextView test3 = (TextView) findViewById(R.id.TextView2);
+        test3.setText(ana_yemek);
+        TextView test4 = (TextView) findViewById(R.id.TextView3);
+        test4.setText(tatli);
+        TextView test5 = (TextView) findViewById(R.id.TextView4);
+        test5.setText(icecek);
+
+
+
+
 
         String baslik2 = getIntent().getExtras().getString("mus2");
         String baslangic2 = getIntent().getExtras().getString("bas2");
@@ -32,8 +44,16 @@ public class DetayActivity extends AppCompatActivity {
         String tatli2 = getIntent().getExtras().getString("tat2");
         String icecek2 = getIntent().getExtras().getString("içe2");
 
-        TextView test2 = (TextView) findViewById(R.id.test2);
-        test2.setText(baslangic2);
+        TextView test6 = (TextView) findViewById(R.id.Baslik2);
+        test6.setText(baslik2);
+        TextView test7 = (TextView) findViewById(R.id.TextView5);
+        test7.setText(baslangic2);
+        TextView test8 = (TextView) findViewById(R.id.TextView6);
+        test8.setText(ana_yemek2);
+        TextView test9 = (TextView) findViewById(R.id.TextView7);
+        test9.setText(tatli2);
+        TextView test10 = (TextView) findViewById(R.id.TextView8);
+        test10.setText(icecek2);
 
         String baslik3 = getIntent().getExtras().getString("mus3");
         String baslangic3 = getIntent().getExtras().getString("bas3");
@@ -41,8 +61,17 @@ public class DetayActivity extends AppCompatActivity {
         String tatli3 = getIntent().getExtras().getString("tat3");
         String icecek3 = getIntent().getExtras().getString("içe3");
 
-        TextView test3 = (TextView) findViewById(R.id.test3);
-        test3.setText(baslangic3);
+        TextView test11 = (TextView) findViewById(R.id.Baslik3);
+        test11.setText(baslik3);
+        TextView test12 = (TextView) findViewById(R.id.TextView9);
+        test12.setText(baslangic3);
+        TextView test13 = (TextView) findViewById(R.id.TextView10);
+        test13.setText(ana_yemek3);
+        TextView test14 = (TextView) findViewById(R.id.TextView11);
+        test14.setText(tatli3);
+        TextView test15 = (TextView) findViewById(R.id.TextView12);
+        test15.setText(icecek3);
+
 
     }
 }
